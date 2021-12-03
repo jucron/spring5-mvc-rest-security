5d61d8ae4e642fb84a82678e5669a73af3343aaf
jucron <jucron@gmail.com>
2021-12-03 17:37:39 +0000

    Implemented PATCH and UPDATE in CustomerController.

--- src/main/java/guru/springfamework/services/CustomerServiceImpl.java
+++ src/main/java/guru/springfamework/services/CustomerServiceImpl.java
@@ -66,4 +66,25 @@ public class CustomerServiceImpl implements CustomerService {
 
         return returnDTO;
     }
+    //Path operation means that fields that are not given, are not updated
+    //(Different from Update operation!)
+    @Override
+    public CustomerDTO patchCustomer(Long id, CustomerDTO customerDTO) {
+        return customerRepository.findById(id).map(customer -> {
+
+            if(customerDTO.getFirstname() != null){
+                customer.setFirstname(customerDTO.getFirstname());
+            }
+
+            if(customerDTO.getLastname() != null){
+                customer.setLastname(customerDTO.getLastname());
+            }
+            CustomerDTO returnDTO = customerMapper.customerToCustomerDTO(
+                    customerRepository.save(customer));
+            returnDTO.setCustomerUrl(URL+id);
+
+            return returnDTO;
+        }).orElseThrow(RuntimeException::new); //todo implement better exception handling;
+
+    }
 }
